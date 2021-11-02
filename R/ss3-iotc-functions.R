68f23665e4db78f784864a3bce4ac372aad7a1c0
Kathryn Doering <kathryn.doering@noaa.gov>
2021-11-02 16:30:55 -0400

    more work on function to convert 3.24 to 3.30

--- R/ss3-iotc-functions.R
+++ R/ss3-iotc-functions.R
@@ -4,20 +4,21 @@
 
 
 run_1a_analysis <- function() {
-  data("YFT_SRD_1A_4")
+  data("YFT_SRD_1A_4_v2")
   # convert data format from SS 3.24 5o SS 3.30
-  dat_1A_4_330 <- convert_data(dat = dat_1A_4)
+  dat_1A_4_330 <- convert_dat_SS324_to_SS330(dat = dat_1A_4)
 }
 
-convert_dat_SS324_to_SS330 <- function(dat_3.24) {
+convert_dat_SS324_to_SS330 <- function(dat_3.24, ref_dat) {
   if(!is.list(dat_3.24)) {
     dat_3.24 <- r4ss::SS_readdat(file = dat_3.24, version = "3.24",
                                  verbose = FALSE)
   }
+  browser()
   dat_3.30 <- dat_3.24
   dat_3.30$SSversion <- NULL
   dat_3.30$ReadVersion <- "3.30"
-  dat_3.30$Nsubseasons <- 
+  dat_3.30$Nsubseasons <- 1 # not sure about this.
   dat_3.30$spawn_month <- dat_3.24$spawn_seas # note: may need something more sophisticated 
   dat_3.30$spawn_seas <- NULL
   dat_3.30$Nfleets <- dat_3.24$Nfleet + dat_3.24$Nsurveys
@@ -38,4 +39,24 @@ convert_dat_SS324_to_SS330 <- function(dat_3.24) {
   colnames(tmp_catch)[4] <- "catch"
   tmp_catch$catch_se <- 0.01 # is this correct? I think this info is missing from SS 3.24.
   dat_3.30$catch <- tmp_catch
+  
+  # cpue ----
+  dat_3.30$CPUEinfo$SD_Report <- 0 
+  rownames(dat_3.30$CPUEinfo) <- dat_3.30$fleetnames
+  browser()
+  # discards ok as is
+  dat_3.30$CPUE # not sure where the fleet is here?
+  dat_3.30$use_meanbodywt <- ifelse(dat_3.30$N_meanbodywt > 0, 1, 0)
+  if(dat_3.30$use_meanbodywt > 0) {
+    stop("code not added to translate mean body weight")
+  }
+  dat_3.30$N_meanbodywt <- NULL
+  dat_3.30$DF_for_meanbodywt <- NULL
+  # pop bin setup ----
+  # the same, at least for method 2
+  # len comp ----
+  dat_3.30$use_lencomp <- ifelse(dat_3.30$N_lencomp > 0, 1, 0)
+  head(dat_3.30$lencomp)
+  
+  
 }
